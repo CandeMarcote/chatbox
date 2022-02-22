e51c0b9debfbae3157f584d7722eb3f58bfb90c6
Candelaria Marcote <candemarcote@gmail.com>
2022-02-21 22:13:28 -0300

    styles incorporated. Minor changes left

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 const socket = io('http://localhost:3000');
-const messageContainer = document.getElementById('message-container')
+const chatbox = document.querySelector('#chatbox')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
 const username = prompt("what is your name?");
-appendMessage('you joined')
+appendSystemMessage('~ you joined ~')
 socket.emit('new-user', username)
 
 socket.on('chat-message', data => {
-    appendMessage(`${data.username}: ${data.message}`)
+    appendReceivedMessage(`${data.username}: ${data.message}`)
 })
 
 socket.on('user-connected', username => {
-    appendMessage(`${username} joined the chat`)
+    appendSystemMessage(`${username} joined the chat`)
 })
 
 socket.on('user-disconnected', username => {
-    appendMessage(`${username} left the chat`)
+    appendSystemMessage(`${username} left the chat`)
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message =  messageInput.value;
-    appendMessage(`You: ${message}`)
+    appendSentMessage(`You: ${message}`)
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
 
-function appendMessage (message) {
-    const messageElement = document.createElement('div')
+function appendSentMessage (message) {
+    const sentContainer = document.createElement('div');
+    sentContainer.classList.add("sent-container");
+
+    const messageElement = document.createElement('p')
+    messageElement.classList.add("sent", "message")
     messageElement.innerText = message
-    messageContainer.append(messageElement)
+
+    sentContainer.append(messageElement)
+    chatbox.append(sentContainer)
+}
+
+function appendReceivedMessage (message) {
+    const receivedContainer = document.createElement('div');
+    receivedContainer.classList.add("received-container");
+
+    const messageElement = document.createElement('p')
+    messageElement.classList.add("received", "message")
+    messageElement.innerText = message
+
+    receivedContainer.append(messageElement)
+    chatbox.append(receivedContainer)
+}
+
+function appendSystemMessage (message) {
+    const messageElement = document.createElement('p')
+    messageElement.classList.add("system")
+    messageElement.innerText = message
+    chatbox.append(messageElement)
 }
